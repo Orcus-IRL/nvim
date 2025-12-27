70df90e7084e7a6e43a1be4bda45499ab531b19d
Orcus-IRL <vkarthick986@gmail.com>
2025-12-27 14:04:21 +0530

    keeping only the one that I'm currently using

--- init.lua
+++ init.lua
@@ -1,17 +1,19 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
+-- local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+-- if not (vim.uv or vim.loop).fs_stat(lazypath) then
+--   vim.fn.system({
+--     "git",
+--     "clone",
+--     "--filter=blob:none",
+--     "https://github.com/folke/lazy.nvim.git",
+--     "--branch=stable", -- latest stable release
+--     lazypath,
+--   })
+-- end
+-- vim.opt.rtp:prepend(lazypath)
+--
+-- require("vim-options")
+-- require("lazy").setup("plugins")
 
 require("vim-options")
-require("lazy").setup("plugins")
-
+require("plugins")
 -- TODO: Add a plugin for github 
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -5,7 +5,7 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set number")
 vim.cmd("set relativenumber")
---vim.opt.clipboard = "unnamedplus"
+vim.opt.clipboard = "unnamedplus"
 vim.o.timeout = true
 vim.o.timeoutlen = 300
 vim.o.mouse = ''
